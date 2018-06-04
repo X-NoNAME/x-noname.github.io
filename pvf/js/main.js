d5e3db45c9e362b0fa517ba400fd20862333d98b
Ivan Kazakov <i.kazakov@corp.badoo.com>
2018-06-04 15:32:17 +0300

    5

--- pvf/js/main.js
+++ pvf/js/main.js
@@ -5,32 +5,56 @@
  */
 
 
-function get_cookie ( cookie_name )
+function get_cookie(cookie_name)
 {
-  var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
- 
-  if ( results )
-    return ( unescape ( results[2] ) );
-  else
-    return null;
+    var results = document.cookie.match('(^|;) ?' + cookie_name + '=([^;]*)(;|$)');
+
+    if (results)
+        return (unescape(results[2]));
+    else
+        return null;
 }
 
 function setHeader(xhr) {
-        xhr.setRequestHeader('Authorization', 'OAuth '+get_cookie('yat'));
-    }
+    xhr.setRequestHeader('Authorization', 'OAuth ' + get_cookie('yat'));
+}
 
-function go(){
-     $.ajax({
-          url: 'https://cloud-api.yandex.net/v1/disk/resources/last-uploaded',
-          type: 'GET',
-          contentType: 'application/json',
-          dataType: "json",
-          data: {limit: 10, media_type: 'image'},
-          success: function(data) { 
-                console.log(data);
-            },
-          error: function(data) { console.log('ERROR', data); },
-          beforeSend: setHeader
-        });
-    
+function go() {
+    getFolders();
+}
+
+function getFiles(folder) {
+    $.ajax({
+        url: 'https://cloud-api.yandex.net/v1/disk/resources',
+        type: 'GET',
+        contentType: 'application/json',
+        dataType: "json",
+        data: { path: folder, fields: 'total'},
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (data) {
+            console.log('ERROR', data);
+        },
+        beforeSend: setHeader
+    });
+
+}
+
+function getFolders() {
+    $.ajax({
+        url: 'https://cloud-api.yandex.net/v1/disk/',
+        type: 'GET',
+        contentType: 'application/json',
+        dataType: "json",
+        data: {},
+        success: function (data) {
+            console.log(data.system_folders.photostream);
+            getFiles(data.system_folders.photostream);
+        },
+        error: function (data) {
+            console.log('ERROR', data);
+        },
+        beforeSend: setHeader
+    });
 }
