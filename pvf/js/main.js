230e5a5b1a5f98f79d90e4b0a2b836b6c6d89d98
Ivan Kazakov <i.kazakov@corp.badoo.com>
2018-06-04 16:18:54 +0300

    5

--- pvf/js/main.js
+++ pvf/js/main.js
@@ -23,15 +23,51 @@ function go() {
     getFolders();
 }
 
+function showRandom(folder,total) {
+    
+    var pos = Math.floor(Math.random() * total+1); 
+    
+    $.ajax({
+        url: 'https://cloud-api.yandex.net/v1/disk/resources',
+        type: 'GET',
+        contentType: 'application/json',
+        dataType: "json",
+        data: { path: folder, limit:1, offset:pos, fields:'_embedded.items.media_type,_embedded.items.file,_embedded.items.name' },
+        success: function (data) {
+            console.log('File',data);
+            if(data._embedded.items[0]){
+                var name = data._embedded.items[0].name;
+                var path = data._embedded.items[0].file;
+                var media_type = data._embedded.items[0].media_type;
+                var content = $("#content");
+                content.html('');
+                if(media_type=="image"){
+                    $("<img/>",{src:path, title:name}).appendTo(content);
+                }else if(media_type=="video"){
+                    $("<video/>",{src:path, title:name, autoplay:"autoplay"}).appendTo(content);
+                }
+                setTimeout(showRandom, 30000, folder, total);
+            }
+            
+        },
+        error: function (data) {
+            console.log('ERROR', data);
+        },
+        beforeSend: setHeader
+    });
+}
+
 function getFiles(folder) {
     $.ajax({
         url: 'https://cloud-api.yandex.net/v1/disk/resources',
         type: 'GET',
         contentType: 'application/json',
         dataType: "json",
-        data: { path: folder, fields: 'total'},
+        data: { path: folder, fields: '_embedded.total'},
         success: function (data) {
             console.log(data);
+            var total = data._embedded.total;
+            showRandom(folder,total);
         },
         error: function (data) {
             console.log('ERROR', data);
@@ -50,7 +86,12 @@ function getFolders() {
         data: {},
         success: function (data) {
             console.log(data.system_folders.photostream);
-            getFiles(data.system_folders.photostream);
+            if(data.system_folders.photostream){
+                getFiles(data.system_folders.photostream);
+            }else {
+                console.log('Can\'t find data.system_folders.photostream');
+            }
+            
         },
         error: function (data) {
             console.log('ERROR', data);
